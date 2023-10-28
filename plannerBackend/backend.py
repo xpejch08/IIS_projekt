da470fda3ec8d226bde94fa9c49d69890adb595c
xpejch08 <pejcharstepan@gmail.com>
2023-10-29 01:56:31 +0200

    create user, delete user, get user working

--- plannerBackend/backend.py
+++ plannerBackend/backend.py
@@ -1,58 +1,21 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from plannerBackend.db import db
+from plannerBackend.routes import my_routes
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://username:password@localhost/userdb'
-db = SQLAlchemy(app)
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/iisdatabase'
+  # MySQL database
+    db.init_app(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    with app.app_context():
+        db.create_all()
 
-    def as_dict(self):
-       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+    app.register_blueprint(my_routes)
 
-@app.route('/users', methods=['GET'])
-def get_users():
-    users = User.query.all()
-    return jsonify([user.as_dict() for user in users])
+    return app
 
-@app.route('/users/<int:user_id>', methods=['GET'])
-def get_user(user_id):
-    user = User.query.get(user_id)
-    if user is None:
-        return jsonify({'error': 'User not found'}), 404
-    return jsonify(user.as_dict())
-
-@app.route('/users', methods=['POST'])
-def create_user():
-    data = request.get_json()
-    new_user = User(username=data['username'], email=data['email'])
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify(new_user.as_dict()), 201
-
-@app.route('/users/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
-    user = User.query.get(user_id)
-    if user is None:
-        return jsonify({'error': 'User not found'}), 404
-    data = request.get_json()
-    user.username = data['username']
-    user.email = data['email']
-    db.session.commit()
-    return jsonify(user.as_dict())
-
-@app.route('/users/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    user = User.query.get(user_id)
-    if user is None:
-        return jsonify({'error': 'User not found'}), 404
-    db.session.delete(user)
-    db.session.commit()
-    return '', 204
 
 if __name__ == '__main__':
-    db.create_all()  # Creates tables on startup
+    app = create_app()
     app.run(debug=True)
---
+++ plannerBackend/tests/test.py
@@ -0,0 +1,73 @@
+import unittest
+import subprocess
+import json
+import time
+import requests
+
+class TestUserRoutes(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # Start the Flask backend in a separate process
+        cls.flask_process = subprocess.Popen(['python', '../backend.py'])
+
+        # Give some time for the Flask app to start (you may need to adjust the sleep time)
+        time.sleep(2)
+
+    @classmethod
+    def tearDownClass(cls):
+        # Terminate the Flask backend process when tests are finished
+        cls.flask_process.terminate()
+        cls.flask_process.wait()
+
+    def test_create_user(self):
+        # Test creating a new user
+        data = {
+            'name': 'newuser',
+            'password': 'password123',
+            'role': 'user',
+        }
+
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post('http://localhost:5000/createUser', json=data, headers=headers)
+
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_user_invalid_data(self):
+        # Test creating a user with missing data
+        data = {
+            'name': 'newuser',
+        }
+
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post('http://localhost:5000/createUser', json=data, headers=headers)
+
+        self.assertEqual(response.status_code, 500)
+
+    def test_update_user(self):
+        # Test updating a user
+        data = {
+            'name': 'newuser',
+            'password': 'updatedpassword',
+            'role': 'admin',  # Update the role
+        }
+
+        headers = {'Content-Type': 'application/json'}
+        response = requests.put('http://localhost:5000/updateUser/1', json=data, headers=headers)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_user(self):
+        # Test deleting a user
+        response = requests.delete('http://localhost:5000/deleteUser/newuser')
+
+        self.assertEqual(response.status_code, 204)  # Use 204 for a successful delete
+
+    def test_get_user(self):
+        # Test getting a user
+        response = requests.get('http://localhost:5000/getUser/1')
+
+        self.assertEqual(response.status_code, 200)  # Use 200 for a successful get
+
+if __name__ == '__main__':
+    unittest.main()
